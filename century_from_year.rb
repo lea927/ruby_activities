c3a6ac5b5d6bba3d3cfec6d8f72d9086f6fe0a77
lea927 <lxthym@icloud.com>
2021-07-13 12:02:24 +0800

    Adds instruction and test snippet

---
+++ century_from_year.rb
@@ -0,0 +1,11 @@
+# CENTURY FROM YEAR
+# The first century spans from the year 1 up to and including the year 100, The second - from the year 101 up to and including the year 200, etc.
+# TASK:
+# Given a year, return the century it is in.
+
+century_from_year(1705) == 18
+century_from_year(1900) == 19
+century_from_year(1601) == 17
+century_from_year(2000) == 20
+
+ 
